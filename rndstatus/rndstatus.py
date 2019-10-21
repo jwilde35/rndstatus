22c57370d804f9e3901e7b684c4bf6fcbef689bf
jwilde35 <jwilde35@gmail.com>
2019-10-20 23:45:11 -0400

    Update rndstatus.py

--- rndstatus/rndstatus.py
+++ rndstatus/rndstatus.py
@@ -46,8 +46,8 @@ class RandomStatus(BaseCog):
         if statuses == () or "" in statuses:
             await ctx.author.send("Current statuses: " + " | ".join(self.statuses))
             return
-        statuses = statuses.split(", ")
-        await self.config.statuses.set(statuses)
+        stat = list(statuses)
+        await self.config.statuses.set(stat)
         await self.bot.change_presence(status=current_status)
         await ctx.send("Done. Redo this command with no parameters to see the current list of statuses.")
 
